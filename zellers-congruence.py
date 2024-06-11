ff82b35fa4dc2c42ffdfb45495e9b83d8be21d6e
gustavoreislima13 <151890840+andremarko@users.noreply.github.com>
2024-06-11 16:53:41 -0300

    Basic upgrade now using datetime library.

--- zellers-congruence.py
+++ zellers-congruence.py
@@ -1,3 +1,5 @@
+import datetime
+
 # Zeller's Congruence Algorithm
      
 months = { 
@@ -25,30 +27,42 @@ day_of_the_week = {
     6: "Friday"    
 }
 
-day = int(input("Insert a day: "))
-month = int(input("Insert a month: "))
-year = int(input("Insert a year: "))
+while (True):
+    
+    try:
+        insert_date = input("Insert a date (dd-MM-yyyy): ")
 
-# Adjust for January and February -- Formula
-if month == 1 or month == 2:    
-    month += 12
-    year -=1
+        parsed_date = datetime.datetime.strptime(insert_date, "%d-%m-%Y")
 
-# Extract the last two digits of the year    
-k = year % 100
-# Extract the century
-cent = year // 100
+        day = parsed_date.day
+        month = parsed_date.month
+        year = parsed_date.year
+        
+        # Adjust for January and February --> Formula
+        if month == 1 or month == 2:    
+            month += 12
+            year -=1
 
-# Zeller's Congruence Formula
-x = (day + ((month + 1) * 26 // 10) + k + (k // 4) +(cent // 4) - 2 * cent) % 7
+        # Extract the last two digits of the year    
+        k = year % 100
+        # Extract the century
+        cent = year // 100
 
-# Display day of the week - dict 
-day_week = day_of_the_week[x]
+        # Zeller's Congruence Formula
+        x = (day + ((month + 1) * 26 // 10) + k + (k // 4) +(cent // 4) - 2 * cent) % 7
 
-# Display month
-display_month = months[month]
+        # Display day of the week - dict 
+        day_week = day_of_the_week[x]
 
+        # Display month
+        display_month = months[month]
 
-print(f"{display_month} {day}, {year + 1 if month > 12 else year} was a {day_week}")
+        print(f"{display_month} {day}, {year + 1 if month > 12 else year} {'was a' if parsed_date < datetime.datetime.now() else 'is going to be a'} {day_week}")
+                
+        continue
+            
+    except ValueError:
+        print("Invalid date, please retry")
+                        
 
 
